3403c4c41fc7115bc331b91bfd43e327bf0a259d
michaelmeir <99044520@mydavinci.nl>
2019-02-06 10:34:47 +0100

    added the additional requirements

--- main.js
+++ main.js
@@ -14,8 +14,14 @@ var pro = document.getElementById("btn_pro");
 var amb = document.getElementById("btn_amb");
 var con = document.getElementById("btn_con");
 
+var btn_parties = document.getElementById("parties");
+
 var btn_skip = document.getElementById("btn_skip");
 var btn_back = document.getElementById("btn_back");
+
+var extra_weight = document.getElementById("extra_weight");
+
+var show_parties = 0
 
 window.onload = () => {
 	btn_start.onclick = start;
@@ -26,6 +32,24 @@ window.onload = () => {
 
 	btn_back.onclick = back;
 	btn_skip.onclick = skip;
+
+	btn_parties.onclick = () => {
+		show_parties++
+		if(show_parties > 5) {
+			show_parties = 0
+		}
+
+		let text = ""
+		switch(show_parties) {
+			case 0: text = "Alle partijen"; break;
+			case 1: text = "Alleen seculaire partijen"; break;
+			case 2: text = "Alleen grote partijen"; break;
+			case 3: text = "Alleen kleine partijen"; break;
+			case 4: text = "Alleen Grote en seculaire partijen"; break;
+			case 5: text = "Alleen Kleine en seculaire partijen"; break;
+		}
+		btn_parties.innerHTML = text;
+	}
 }
 
 let map = null;
@@ -44,10 +68,17 @@ function start() {
 
 function next(answer) {
 	if(map == null || curr_question == null) return;
+	curr_question.extra_weight = extra_weight.checked
 	map.AddAnswer(curr_question, answer)
 	curr_question = NewQuestion(map)
 	if(curr_question == null) {
 		OutputResult()
+	}else{
+		if(curr_question.extra_weight == undefined || curr_question.extra_weight == false) {
+			extra_weight.checked = false;
+		}else{
+			extra_weight.checked = true;
+		}
 	}
 }
 
@@ -90,7 +121,7 @@ function NewQuestion(map) {
 function OutputResult() {
 	hide(question_panel)
 	reveal(result_panel)
-	map.RevealStats(map.Output(), results, subjects.length);
+	map.RevealStats(map.Output(), results);
 }
 
 
@@ -130,12 +161,12 @@ function Mapping() {
 				let exists = false
 				for (var k = parties.length - 1; k >= 0; k--) {
 					if(q.parties[j].name == parties[k].name) {
-						parties[k].points += this.CalcPoint(q.parties[j].position, q.answer)
+						parties[k].points += q.extra_weight ? this.CalcPoint(q.parties[j].position, q.answer) *2 : this.CalcPoint(q.parties[j].position, q.answer)
 						exists = true
 					}
 				}
 				if(!exists) {
-					q.parties[j].points = this.CalcPoint(q.parties[j].position, q.answer)
+					q.parties[j].points = q.extra_weight ? this.CalcPoint(q.parties[j].position, q.answer) *2 : this.CalcPoint(q.parties[j].position, q.answer)
 					parties.push(q.parties[j])
 				}
 			}
@@ -159,16 +190,31 @@ function Mapping() {
 		return -1
 	}
 
-	this.RevealStats = (output, element, maxPoints) => {
+	this.RevealStats = (output, element) => {
 		element.innerHTML = ""
 		output.sort(function(a, b) {return a.points - b.points})
+		var weight_count = 0;
+		for (var i = this.questions.length - 1; i >= 0; i--) {
+			weight_count++
+			if(this.questions[i].extra_weight) weight_count++
+		}
 		let stat = (per, txt, max) => { 
 			return per >= 0 ? 
 				"<div class=\"w3-animate-right w3-light-grey\"><div class=\"w3-container w3-green w3-center w3-text-black\" style=\"width: " + Math.floor((per*100)/max) + "%\">" + txt + ": " + Math.floor((per*100)/max) + "%</div></div>"
 		 	  : "<div class=\"w3-animate-right w3-light-grey\"><div class=\"w3-container w3-red w3-center w3-text-black\" style=\"width: " + Math.floor((-per*100)/max) + "%\">" + txt + ": -" + Math.floor((-per*100)/max) + "%</div></div>" 
 		 	}
 		for (var i = output.length - 1; i >= 0; i--) {
-			element.innerHTML += stat(output[i].points, output[i].name, maxPoints)
+			for (var j = parties.length - 1; j >= 0; j--) {
+				if(parties[j].name == output[i].name && 
+					((show_parties == 1 && parties[j].secular == true) || 
+						(show_parties == 2 && parties[j].size > 5) || 
+						show_parties == 0) ||
+					(show_parties == 3 && parties[j].size <= 5) ||
+					(show_parties == 4 && parties[j].size > 5 && parties[j].secular) ||
+					(show_parties == 5 && parties[j].size <= 5 && parties[j].secular)) {
+					element.innerHTML += stat(output[i].points, output[i].name, weight_count)
+				}
+			}
 		}
 	}
 }
